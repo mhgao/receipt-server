f3ff256722c5f0e0afaab8c50995e41f991b8e01
Martin Gao <martin.gao@tufts.edu>
2021-01-24 12:09:22 -0500

    Removed driver code from scan.py

--- image_scanner/scan.py
+++ image_scanner/scan.py
@@ -353,37 +353,3 @@ class DocScanner(object):
         # basename = os.path.basename(image_path)
         # cv2.imwrite(OUTPUT_DIR + "/" + basename, thresh)
         # print("Proccessed " + basename)
-
-
-if __name__ == "__main__":
-    ap = argparse.ArgumentParser()
-    group = ap.add_mutually_exclusive_group(required=True)
-    group.add_argument("--images", help="Directory of images to be scanned")
-    group.add_argument("--image", help="Path to single image to be scanned")
-    ap.add_argument(
-        "-i",
-        action="store_true",
-        help="Flag for manually verifying and/or setting document corners",
-    )
-
-    args = vars(ap.parse_args())
-    im_dir = args["images"]
-    im_file_path = args["image"]
-    interactive_mode = args["i"]
-
-    scanner = DocScanner(interactive_mode)
-
-    valid_formats = [".jpg", ".jpeg", ".jp2", ".png", ".bmp", ".tiff", ".tif"]
-
-    get_ext = lambda f: os.path.splitext(f)[1].lower()
-
-    # Scan single image specified by command line argument --image <IMAGE_PATH>
-    if im_file_path:
-        scanner.scan(cv2.imread(im_file_path))
-
-    # Scan all valid images in directory specified by command line argument --images <IMAGE_DIR>
-    else:
-        im_files = [f for f in os.listdir(im_dir) if get_ext(f) in valid_formats]
-        for im in im_files:
-            scanner.scan(im_dir + "/" + im)
-
